eadd55f65bef2d10fc5c19d102a92ba9b3f64dfe
leandroalfonso <68873104+leandroalfonso@users.noreply.github.com>
2022-11-27 08:01:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# todo-list-leandro-9kzokc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/todo-list-leandro-9kzokc)
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [task, setTask] = useState([]);
+
+  function add() {
+    setTask([...task, input]);
+    setInput("")
+    localStorage.setItem("task",JSON.stringify(task))
+  
+  }
+
+
+
+  return (
+    <div className="App">
+      <div className="container">
+        <input
+          type="text"
+          placeholder="digite sua tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={add}>enviar</button>
+      </div>
+      <ul>{
+      
+      task.map((tar)=>(
+        <li key={tar}>{tar}</li>
+
+      ))
+      
+      
+      }</ul>
+    </div>
+  );
+}
+
+export default App;
